b970d8ca6ac40e293d920a222c8857dea6ddbfe0
SebastianIRodriguez <48632951+SebastianIRodriguez@users.noreply.github.com>
2019-11-04 20:31:43 -0300

    correccion

--- src/Colectivo.php
+++ src/Colectivo.php
@@ -33,36 +33,37 @@ class Colectivo implements ColectivoInterface {
         
         if (
             ($tarjeta->tipotarjeta() != 'medio universitario') && 
-            ($tarjeta->tipotarjeta() != 'media franquicia estudiantil') &&
-            ($tarjeta->pagar($this) == true)) {
+            ($tarjeta->tipotarjeta() != 'media franquicia estudiantil')){
+            if ($tarjeta->pagar($this) == true)) {
                 
-            if ($tarjeta->usoplus() == true) {
-                $boleto = new Boleto('0.0', $this, $tarjeta, 'viaje plus', " ");
-                $tarjeta->guardarUltimoBoleto($boleto);
-                return $boleto;
+                if ($tarjeta->usoplus() == true) {
+                    $boleto = new Boleto('0.0', $this, $tarjeta, 'viaje plus', " ");
+                    $tarjeta->guardarUltimoBoleto($boleto);
+                    return $boleto;
 
-            } else if($tarjeta->devolverUltimoTransbordo()){
-                $boleto = new Boleto($tarjeta->devolverUltimoPago(), $this, $tarjeta, "TRANSBORDO", " ");
-                $tarjeta->guardarUltimoBoleto($boleto);
-                return $boleto;
-            }
-            else if ($tarjeta->MostrarPlusDevueltos() == 0) {
-                $boleto = new Boleto(
-                    $tarjeta->devolverUltimoPago(), 
-                    $this,
-                    $tarjeta,
-                    $tarjeta->tipotarjeta(), " ");
-                $tarjeta->guardarUltimoBoleto($boleto);
-                return $boleto;
-            }
-            else {
-                $boleto = new Boleto(
-                    $tarjeta->devolverUltimoPago(), 
-                    $this, 
-                    $tarjeta, $tarjeta->tipotarjeta(),
-                    "Paga " . (string) $tarjeta->MostrarPlusDevueltos() . " Viaje Plus");
-                $tarjeta->guardarUltimoBoleto($boleto);
-                return $boleto;
+                } else if($tarjeta->devolverUltimoTransbordo()){
+                    $boleto = new Boleto($tarjeta->devolverUltimoPago(), $this, $tarjeta, "TRANSBORDO", " ");
+                    $tarjeta->guardarUltimoBoleto($boleto);
+                    return $boleto;
+                }
+                else if ($tarjeta->MostrarPlusDevueltos() == 0) {
+                    $boleto = new Boleto(
+                        $tarjeta->devolverUltimoPago(), 
+                        $this,
+                        $tarjeta,
+                        $tarjeta->tipotarjeta(), " ");
+                    $tarjeta->guardarUltimoBoleto($boleto);
+                    return $boleto;
+                }
+                else {
+                    $boleto = new Boleto(
+                        $tarjeta->devolverUltimoPago(), 
+                        $this, 
+                        $tarjeta, $tarjeta->tipotarjeta(),
+                        "Paga " . (string) $tarjeta->MostrarPlusDevueltos() . " Viaje Plus");
+                    $tarjeta->guardarUltimoBoleto($boleto);
+                    return $boleto;
+                }
             }
         }
         else if ($tarjeta->pagoMedioBoleto($this) == TRUE) {
