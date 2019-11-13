f565f0a9d4a21943e46fb7a5e333f35e5d2e3278
SebastianIRodriguez <48632951+SebastianIRodriguez@users.noreply.github.com>
2019-11-13 01:18:42 -0300

    movido transbordo a trait

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -37,24 +37,6 @@ class Tarjeta implements TarjetaInterface {
         return ($this->saldo >= ($this->monto + $this->viajeplus * Tarifas::boleto));
     }
 
-
-    public function tiempoTransbordo() {
-        if ($this->tiempo->esDiaSemana() && $this->tiempo->esFeriado() == FALSE) {
-            return 60 * 60;
-        }
-        return 120 * 60;
-    }
-
-    public function esTransbordo(Colectivo $colectivo) {
-        return (
-            ($this->ultimoViaje == null ||
-            $colectivo->linea() != $this->ultimoViaje->getLinea()) &&
-            $this->ultimoViaje->getTipo() != TipoViaje::TRANSBORDO &&
-            $this->ultimoViaje->getTipo() != TipoViaje::VIAJE_PLUS &&
-            $this->tiempo->getTiempo() - $this->ultimoViaje->getTiempo() < $this->tiempoTransbordo());
-    }
-
-
     public function pagar(Colectivo $colectivo) {
 
         $sePudoPagar = false;
@@ -68,7 +50,7 @@ class Tarjeta implements TarjetaInterface {
                 $montoAPagar = $this->monto;
                 $tipo = TipoViaje::NORMAL;
             }
-            elseif ($this->esTransbordo($colectivo)) {
+            elseif ($this->esTransbordo($colectivo, $this->tiempo, $this->ultimoViaje)) {
                 $montoAPagar = Tarifas::transbordo;
                 $tipo = TipoViaje::TRANSBORDO;
             }
---
+++ src/TransbordoTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+trait TransbordoTrait {
+    public function esTransbordo(Colectivo $colectivo, TiempoInterface $tiempo, Viaje $viaje) {
+        return (
+            ($viaje == null ||
+            $colectivo->linea() != $viaje->getLinea()) &&
+            $viaje->getTipo() != TipoViaje::TRANSBORDO &&
+            $viaje->getTipo() != TipoViaje::VIAJE_PLUS &&
+            $tiempo->getTiempo() - $viaje->getTiempo() < $this->tiempoTransbordo($tiempo));
+    }
+
+    public function tiempoTransbordo($tiempo) {
+        if ($tiempo->esDiaSemana() && $tiempo->esFeriado() == FALSE) {
+            return 60 * 60;
+        }
+        return 120 * 60;
+    }
+
+}
